1982038b84947b0c67ed8aca4ca8a0032a210d92
Gilbert Lundberg Rydbäck <gilbert.lundberg.rydback@iths.se>
2023-01-19 05:22:39 +0100

    Changes

--- CRUD-MongoDB/Program.cs
+++ CRUD-MongoDB/Program.cs
@@ -8,7 +8,7 @@
             IDAO productDAO; 
 
             io = new TextIO(); 
-            productDAO = new MongoDAO("mongodb+srv://new_user:new_password@cluster0.ta43dc4.mongodb.net/?retryWrites=true&w=majority", "SneakerStore");
+            productDAO = new MongoDAO("", "SneakerStore");
 
             InventoryController controller = new InventoryController(io, productDAO); 
             controller.Start(); 
